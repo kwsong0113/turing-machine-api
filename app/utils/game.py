b9556bd9f1b11d44de085bac0947e53f5ccdae16
Kiwhan Song <kwsong0113@gmail.com>
2023-08-01 20:45:22 +0900

    chore: cleanup game utilities

--- app/utils/game.py
+++ app/utils/game.py
@@ -1,8 +1,6 @@
-from fastapi import Depends
-
-from app.crud import GameCRUD, get_game_crud
+from app.crud import GameCRUD
 from app.models import GameStatus
 
 
-async def isolate_game(game_id: int, game_crud) -> None:
+async def isolate_game(game_id: int, game_crud: GameCRUD) -> None:
     await game_crud.update(game_id, {"status": GameStatus.PLAYING})
